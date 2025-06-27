62af767d65687ef539a3691c3311bd5775ae5051
wrshf7 <wrshf7@gmail.com>
2025-06-27 17:55:49 +1000

    Modularized, functioning haploblocking function

--- def_haploblocks.R
+++ def_haploblocks.R
@@ -19,7 +19,7 @@ ld[,4:5] = lapply(ld[,4:5], as.character)
 
 #####function to extend the block left####
 
-extend_left = function(first.mkr.blk, snps.chr, assigned, ld.chr, block, method){
+extend_left = function(first.mkr.blk, snps.chr, assigned, ld.chr, block, method, threshold, tolerance){
   
   #repeat left extension until break point is reached (tolerance and threshold)
   repeat {
@@ -31,7 +31,7 @@ extend_left = function(first.mkr.blk, snps.chr, assigned, ld.chr, block, method)
     if (first.idx == 1) break
     
     #get index of marker to the left of the first SNP in the block
-    left.mkr <- snps.chr[first.idx - 1]
+    left.mkr = snps.chr[first.idx - 1]
     
     #if marker to the left is already in a block, break
     if (assigned[left.mkr]) break
@@ -39,23 +39,23 @@ extend_left = function(first.mkr.blk, snps.chr, assigned, ld.chr, block, method)
     #if utilizing the flanking method, pull the LD of the SNP to the left idetnified from left.mkr
     if (method=="flanking") {
       #singular value?
-      ld.vals <- ld.chr[ld.chr$Name1 == left.mkr & ld.chr$Name2 == first.mkr.blk, "LD"]
+      ld.vals = ld.chr[ld.chr$Name1 == left.mkr & ld.chr$Name2 == first.mkr.blk, "LD"]
     }
     
     #if utilizing the average method, then compute the average LD of the new SNP considered with all markers in the block currently
     if (method=="average") {
       #could be multiple values if the block is greater than
-      ld.vals <- ld.chr[ld.chr$Name1 == left.mkr & ld.chr$Name2 %in% block, "LD"]
+      ld.vals = ld.chr[ld.chr$Name1 == left.mkr & ld.chr$Name2 %in% block, "LD"]
     }
     
     #check that the LD value (flanking) or average LD value (average) meets the threshold - mean of a single marker is itself, so it works for both
-    if (mean(ld.vals$LD) <= threshold) break
+    if (mean(ld.vals) <= threshold) break
     
     #if the threshold is met, add it to the block
     block <- c(left.mkr, block)
     
     #update the first marker and repeat the loop
-    #first.mkr.blk <- left.mkr
+    first.mkr.blk <- left.mkr
   }
   
   
@@ -65,39 +65,39 @@ extend_left = function(first.mkr.blk, snps.chr, assigned, ld.chr, block, method)
 
 #####function to extend the block right#####
 
-extend_right = function(last.mkr.blk, snps.chr, assigned, ld.chr, block, method){
+extend_right = function(last.mkr.blk, snps.chr, assigned, ld.chr, block, method, threshold, tolerance){
   
   #keep extending until the break signal is initiated
   repeat{
     #starting from the second marker of the original pair (last marker)
-    last.idx <- match(last.mkr.blk, snps.chr)
+    last.idx = match(last.mkr.blk, snps.chr)
     
     #if it's the last marker in the chromo, don't extend
     if (last.idx == length(snps.chr)) break
     
     #if not, find the next marker
-    right.mkr <- snps.chr[last.idx + 1]
+    right.mkr = snps.chr[last.idx + 1]
     
     #check if the next marker is already in a block
     if (assigned[right.mkr]) break
     
     #same as left extension - pull the LD of the marker to the right or the average of the new marker with the rest of the markers in the block originating from the original pair
     if (method=="flanking") {
-      ld.vals <- ld.chr[ld.chr$Name1 == last.mkr.blk & ld.chr$Name2 == right.mkr, "LD"]
+      ld.vals = ld.chr[ld.chr$Name1 == last.mkr.blk & ld.chr$Name2 == right.mkr, "LD"]
     }
     
     if (method=="average") {
-      ld.vals <- ld.chr[ld.chr$Name1 %in% block & ld.chr$Name2 == right.mkr, "LD"]
+      ld.vals = ld.chr[ld.chr$Name1 %in% block & ld.chr$Name2 == right.mkr, "LD"]
     }
     
     #check to see if it meets threshold criteria
-    if (mean(ld.vals$LD) <= threshold) break
+    if (mean(ld.vals) <= threshold) break
     
     #add it to the block
-    block <- c(block, right.mkr)
+    block = c(block, right.mkr)
     
     #update the last marker
-    #last.mkr.blk <- right.mkr
+    last.mkr.blk <- right.mkr
   }
   return(block)
 }
@@ -105,42 +105,50 @@ extend_right = function(last.mkr.blk, snps.chr, assigned, ld.chr, block, method)
 
 #####function to find a SNP pair to start block extension#####
 
-make_block = function(ld.chr, ld.adj.char, snps.chr, snps.pos.chr, snps.chr, first.mkr.chr, last.mkr.chr, assigned, threshold, tolerance){
+make_blocks = function(ld.chr, ld.adj.chr, snps.chr, snps.pos.chr, first.mkr.chr, last.mkr.chr, assigned, method, threshold, tolerance){
+  
+  chromo_blocks = list()
+  
   #keep making blocks until there is no pair with high enough LD
   repeat {
     
     #identify max LD pair and pull its id
-    max.ld.idx <- which.max(ld.adj.chr$LD)
+    max.ld.idx = which.max(ld.adj.chr$LD)
     
     #extract that LD value and check it
-    max.ld     <- ld.adj.chr$LD[max.ld.idx]
+    max.ld = ld.adj.chr$LD[max.ld.idx]
     if (max.ld <= threshold) break
     
     #Pull the two marker names in the LD pair
-    first.mkr.blk <- as.character(ld.adj.chr$Name1[max.ld.idx])
-    last.mkr.blk  <- as.character(ld.adj.chr$Name2[max.ld.idx])
+    first.mkr.blk = as.character(ld.adj.chr$Name1[max.ld.idx])
+    last.mkr.blk  = as.character(ld.adj.chr$Name2[max.ld.idx])
     
     #if assigned is true for either marker in the LD pair (assigned to a block already), remove it from consideration - updated at the end of the block formation
     if (assigned[first.mkr.blk] || assigned[last.mkr.blk]) {
-      ld.adj.chr <- ld.adj.chr[-max.ld.idx, ]
+      ld.adj.chr = ld.adj.chr[-max.ld.idx, ]
       next
     }
     
     #extend the block
-    block <- c(first.mkr.blk, last.mkr.blk)
+    block = c(first.mkr.blk, last.mkr.blk)
     
     #extend the block
-    extend_left()
-    extend_right()
+    block = extend_left(first.mkr.blk = first.mkr.blk, snps.chr = snps.chr, assigned = assigned, 
+                ld.chr = ld.chr, block = block, method = method, threshold = threshold, tolerance = tolerance)
+    
+    block = extend_right(last.mkr.blk = last.mkr.blk, snps.chr = snps.chr, assigned = assigned, 
+                 ld.chr = ld.chr, block = block, method = method, threshold = threshold, tolerance = tolerance)
     
     #update the assigned variable to indicate that SNP have been assigned to a block
     assigned[block] = TRUE
     
     
-    #blocks_list[[length(blocks_list) + 1]] <- block
+    chromo_blocks[[length(chromo_blocks) + 1]] = block
     
     
-    #ask victor about this code
+    #ask victor about this code - we need to figure out whether we can use this or assigned
+    #probable answer - the last snp doesn't have an "adjacent" snp to the right, so ld.adj.chr has 1 less
+    #snp than snp.chr. The used variable is therefore 1 shorter
     used = with(ld.adj.chr, Name1 %in% block | Name2 %in% block)
     ld.adj.chr = ld.adj.chr[!used, ]
     
@@ -149,11 +157,14 @@ make_block = function(ld.chr, ld.adj.char, snps.chr, snps.pos.chr, snps.chr, fir
     
     
   }
+  
+  return(chromo_blocks)
 }
 
 
 #####master blocking function at the chromosome level#####
-chromo_blocking = function(chr, ld, map, method, tolerance){
+chromo_blocking = function(chr, ld, map, method, tolerance, threshold = threshold){
+  
   #pull the LD information for the chromosome
   ld.chr = ld[ld$Chrom == chr, ]
   
@@ -175,16 +186,19 @@ chromo_blocking = function(chr, ld, map, method, tolerance){
   
   #tracker utilized later to determine whether a SNP has been added to a block
   assigned = rep(FALSE, length(snps.chr))
-  names(assigned) <- snps.chr
+  names(assigned) = snps.chr
   
-  make_block()
+  chromo_blocks = make_blocks(ld.chr = ld.chr, ld.adj.chr = ld.adj.chr, snps.chr = snps.chr, snps.pos.chr = snps.pos.chr,
+             first.mkr.chr = first.mkr.chr, last.mkr.chr = last.mkr.chr, assigned = assigned, method = method, threshold = threshold, tolerance = tolerance)
 
   unassigned = names(assigned)[!assigned]
+  
+  #add these as blocks
   for (snp in unassigned) {
-    blocks_list[[length(blocks_list) + 1]] = snp
+    chromo_blocks[[length(chromo_blocks) + 1]] = snp
   }
   
-  return(blocks_list)
+  return(chromo_blocks)
 }
   
 
@@ -193,14 +207,13 @@ chromo_blocking = function(chr, ld, map, method, tolerance){
 
 
 #####overall function to track progress, take input, and call blocking functions#####
-def_blocks = function(ld, map, method = "flanking", tolerance = 1){
+def_blocks = function(ld, map, method = "flanking", tolerance = 1, threshold = 0.7){
   
-  blocks_list = list()
   #pull the list of chromosomes to parallelize
   chromosomes = sort(unique(ld$Chrom))
   
   #setup parallelization using futures and parallel package and utilize all but 1 core
-  futures::plan(multisession, workers = parallel::detectCores() - 1)
+  #future::plan(multisession, workers = parallel::detectCores() - 1)
   
   #track progress across chromosomes and set up progress bar
   handlers("txtprogressbar")
@@ -210,168 +223,35 @@ def_blocks = function(ld, map, method = "flanking", tolerance = 1){
     p = progressor(steps = length(chromosomes))
     
     #form blocks across chromosomes - parallelize each chromosome
-    furrr::map(chromosomes, function(chr){
+    blocks_list = map(chromosomes, function(chr){
       
       #call chromosome blocking function
-      chromo_blocking(chr, ld, map, method, tolerance)
-      
+      chromo_blocks = chromo_blocking(chr = chr, ld = ld, map = map, method = method, tolerance = tolerance, threshold = threshold)
       #after blocking on the chromosome is finished iterate the progress bar
       p()
+      
+      return(chromo_blocks)
     })
   })
+  #plan(sequential)
+  names(blocks_list) = as.character(chromosomes)
   
-  max_len = max(lengths(blocks_list))
-  blocks_df = as.data.frame(do.call(rbind, lapply(blocks_list, function(x) {
-    length(x) = max_len  # remplit avec NA
-    x
-  })), stringsAsFactors = FALSE)
+  #max_len = max(lengths(blocks_list))
+  #blocks_df = as.data.frame(do.call(rbind, lapply(blocks_list, function(x) {
+  #  length(x) = max_len  # remplit avec NA
+  #  x
+  #})), stringsAsFactors = FALSE)
   
-  blocks_df$chr = map$chrom[match(blocks_df$V1, map$SNP)]
-  blocks_df$pos = map$pos[match(blocks_df$V1, map$SNP)]
-  blocks_df = blocks_df[order(blocks_df$pos),]
+  #blocks_df$chr = map$chrom[match(blocks_df$V1, map$SNP)]
+  #blocks_df$pos = map$pos[match(blocks_df$V1, map$SNP)]
+  #blocks_df = blocks_df[order(blocks_df$pos),]
   
-  return(blocks_df)
+  return(blocks_list)
 }
 
 
+start = Sys.time()
+test = def_blocks(ld = ld, map = map, threshold = 0.5)
+end = Sys.time()
+end - start
 
-def_blocks = function(ld, map, method="none"){
-  
-  blocks_list = list()
-  
-  chromosomes = sort(unique(ld$Chrom))
-  
-  
-  
-  
-  for(chr in chromosomes){print(chr)
-    
-    ld.chr        <- ld[ld$Chrom == chr, ]
-    
-    #doesn't this only consider the immediately adjacent snp? Are blocks being extended based on LD between the new SNP and the original SNP pair (depending on if you're moving left or right) 
-    #or the SNP last incorporated?
-    #I think it needs to be based on the original SNP pair
-    ld.adj.chr    <- ld.chr[ld.chr$Locus2 == ld.chr$Locus1 + 1, ]
-    snps.chr      <- unique(c(ld.chr$Name1, ld.chr$Name2))
-    snps.pos.chr  <- map$pos[match(snps.chr, map$SNP)]
-    snps.chr      <- snps.chr[order(snps.pos.chr)]
-    
-    first.mkr.chr <- snps.chr[1]
-    last.mkr.chr  <- snps.chr[length(snps.chr)]
-    
-    assigned <- rep(FALSE, length(snps.chr))
-    names(assigned) <- snps.chr
-    
-    
-    ## BUILD THE HAPLOBLOCKS
-    ########################
-    repeat {
-     
-      #is this necessary to identify if there is even a pair to advance from first?
-      
-      #identify max LD between a SNP and the one immediately following?
-      #need to consider moving backwards as well?
-      max.ld.idx <- which.max(ld.adj.chr$LD)
-      
-      #extract that LD value and check it
-      max.ld     <- ld.adj.chr$LD[max.ld.idx]
-      if (max.ld <= threshold) break
-      
-      #Pull the two marker names in the LD pair
-      first.mkr.blk <- as.character(ld.adj.chr$Name1[max.ld.idx])
-      last.mkr.blk  <- as.character(ld.adj.chr$Name2[max.ld.idx])
-      
-      #if assigned is true for either marker in the LD pair, remove it from consideration, but when is assigned updated?
-      if (assigned[first.mkr.blk] || assigned[last.mkr.blk]) {
-        ld.adj.chr <- ld.adj.chr[-max.ld.idx, ]
-        next
-      }
-      
-      #extend the block
-      block <- c(first.mkr.blk, last.mkr.blk)
-      
-      # Extension to the left
-      #I think this answers my question about extending to the left
-      repeat {
-        first.idx <- match(first.mkr.blk, snps.chr)
-        if (first.idx == 1) break
-        
-        left.mkr <- snps.chr[first.idx - 1]
-        
-        if (assigned[left.mkr]) break
-        
-        if (method=="flanking") {
-          #singular value?
-          ld.vals <- ld.chr[ld.chr$Name1 == left.mkr & ld.chr$Name2 == first.mkr.blk, "LD"]
-        }
-        if (method=="average") {
-          #could be multiple values?
-          ld.vals <- ld.chr[ld.chr$Name1 == left.mkr & ld.chr$Name2 %in% block, "LD"]
-        }
-        if (mean(ld.vals$LD) <= threshold) break
-        
-        block <- c(left.mkr, block)
-        first.mkr.blk <- left.mkr
-      }
-      
-      # Extension to the right
-      
-      first.mkr.blk = block[1]
-      repeat {
-        
-        #starting from the second marker of the original pair
-        last.idx <- match(last.mkr.blk, snps.chr)
-        if (last.idx == length(snps.chr)) break
-        
-        right.mkr <- snps.chr[last.idx + 1]
-        
-        if (assigned[right.mkr]) break
-        
-        if (method=="flanking") {
-          ld.vals <- ld.chr[ld.chr$Name1 == last.mkr.blk & ld.chr$Name2 == right.mkr, "LD"]
-        }
-        if (method=="average") {
-          ld.vals <- ld.chr[ld.chr$Name1 %in% block & ld.chr$Name2 == right.mkr, "LD"]
-        }
-        
-        
-        if (mean(ld.vals$LD) <= threshold) break
-        
-        block <- c(block, right.mkr)
-        last.mkr.blk <- right.mkr
-      }
-      
-      #start here
-      assigned[block] <- TRUE
-      blocks_list[[length(blocks_list) + 1]] <- block
-      
-      used <- with(ld.adj.chr, Name1 %in% block | Name2 %in% block)
-      ld.adj.chr <- ld.adj.chr[!used, ]
-      if (nrow(ld.adj.chr) == 0) break
-    }
-    
-    
-    ## COMBINE RESULTS 
-    ##################
-    
-    unassigned <- names(assigned)[!assigned]
-    for (snp in unassigned) {
-      blocks_list[[length(blocks_list) + 1]] <- snp
-    }
-    
-    
-  }
-  
-  max_len <- max(lengths(blocks_list))
-  blocks_df <- as.data.frame(do.call(rbind, lapply(blocks_list, function(x) {
-    length(x) <- max_len  # remplit avec NA
-    x
-  })), stringsAsFactors = FALSE)
-  
-  blocks_df$chr = map$chrom[match(blocks_df$V1, map$SNP)]
-  blocks_df$pos = map$pos[match(blocks_df$V1, map$SNP)]
-  blocks_df = blocks_df[order(blocks_df$pos),]
-  
-  
-  return(blocks_df)
-}
