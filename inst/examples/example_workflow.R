3a620f900ffad267f6b7c4e4e5f9bbf181853214
wrshf7 <wrshf7@gmail.com>
2026-04-24 18:54:47 +1000

    Update to exports, update to example scripts (finally), and bug fix in top block selection of GA (% of total blocks specifically)

--- inst/examples/example_workflow.R
+++ inst/examples/example_workflow.R
@@ -2,34 +2,66 @@
 #######  Example Workflow  ##########
 #####################################
 
-# Package functions and required dependencies are provided by HapSelect.
+library(HapSelect)
 
-required_fns <- c(
-  "order_map", "pairwise_ld", "def_blocks", "block_obj_to_df",
-  "compute_local_GEBV", "marker_effects_plot", "unique_haplo_effects_plot",
-  "block_var_funnel_plot", "plot_haploblocks", "plot_marker_density",
-  "plot_ld_decay", "genetic_algorithm"
-)
-
-missing_fns <- required_fns[!vapply(required_fns, exists, logical(1), mode = "function")]
-if (length(missing_fns) > 0) {
-  stop(
-    "Package functions are not loaded. Run devtools::load_all('.') ",
-    "or library(FastStack) before sourcing this script."
-  )
-}
-
-
-#################################
-####  Pairwise LD  ####
-#################################
+#############################################################
+####  Pairwise LD with Native Function (not recommended) ####
+#############################################################
 
 
 #loading map file from an R object
 #the map file chromosomes MUST be numeric!
 #First column should be SNP ID, second column should be chromosome, and third
 #column should be the position
-data("map", package = "FastStack")
+
+data("map", package = "HapSelect")
+
+#simulate unordered map file - original file is already ordered, so this is just an example
+map2 = map[sample(1:nrow(map), nrow(map)), ]
+
+#check that the map file is ordered - using simulated unordered map
+#this function will also check the map file to ensure it is appropriate and set column names
+map2 = order_map(map = map2)
+
+#loading the genotype file from an R object
+#with well-curated marker data, ideally you just set missing genotype values to NA. This would be fine for LD calculations,
+#calculating marker effects, and computing localGEBV
+
+#loading the genotype file from an R object
+#Non-integers represent imputed values (usually to the mean or based on probability of each allele). It is recommended these be imputed to genotype calls
+#or set to missing, otherwise haplotype configurations will be weird values. NOTE: This will not affect localGEBV values.
+data("geno", package = "HapSelect")
+
+
+#compute ld
+
+#first 3 columns of the genotype file should be the map file and the rest of the columns represent accessions/individuals with cells
+#being the genotypes at each locus for each individual
+#column names of the map information (first 3 columns) will not matter in this case, but the order of map info columns
+#should be the same as the map file columns (SNP ID, Chromosome, Position)
+
+#ld_pairs = pairwise_ld(geno, parallelize = FALSE)
+
+#This is very slow and is just for demonstration purposes only!
+
+
+
+#load the example file to see the structure if you do not want to run the function
+data("pairwise_ld", package = "HapSelect")
+
+
+
+####################################################
+####  Pairwise LD with PLINK Call (recommended) ####
+####################################################
+
+
+#loading map file from an R object
+#the map file chromosomes MUST be numeric!
+#First column should be SNP ID, second column should be chromosome, and third
+#column should be the position
+
+data("map", package = "HapSelect")
 
 #simulate unordered map file - original file is already ordered, so this is just an example
 map2 = map[sample(1:nrow(map), nrow(map)), ]
@@ -44,23 +76,28 @@ map2 = order_map(map = map2)
 
 #Non-integers represent imputed values (usually to the mean or based on probability of each allele). These need to be imputed to genotype calls or set to missing.
 
-data("geno", package = "FastStack")
-
-
-#compute ld
-
 #first 3 columns of the genotype file should be the map file and the rest of the columns represent accessions/individuals with cells
 #being the genotypes at each locus for each individual
 #column names of the map information (first 3 columns) will not matter in this case, but the order of map info columns
 #should be the same as the map file columns (SNP ID, Chromosome, Position)
-#ld_pairs = pairwise_ld(geno, parallelize = FALSE)
-
-#This is very slow and is just for demonstration purposes only!
+data("geno", package = "HapSelect")
 
 
+#compute ld with PLINK
+
+
+
+#once LD File Script is updated run this
+ld_pairs = plink_pairwise_ld(prefix = "example_plink", ld_window = 999999, ld_window_kb = 1000000,
+                             ld_window_r2 = 0, extra_args = character())
+
+###########################################
+##### Load LD File (Skip Computation) #####
+###########################################
 
 #load the example file to see the structure if you do not want to run the function
-data("pairwise_ld", package = "FastStack")
+data("pairwise_ld", package = "HapSelect")
+
 
 #note: other programs can be utilized to generate the LD file, but make sure the columns c("Chrom", "Locus1", "Locus2", "Name1", "Name2", "LD")
 #exist in the data frame. We recommend using PLINK v1.9 for LD calculations.
@@ -72,7 +109,7 @@ data("pairwise_ld", package = "FastStack")
 #map data frame by Name1 and SNP and them Name2 and SNP to get the indices for Locus1 and Locus2.
 
 #########################################
-#####  Compute Haploblocks  #####
+########  Compute Haploblocks  ##########
 #########################################
 
 #method = c("flanking", "average") indicates whether to compare the LD threshold between the last marker in a block and the new marker ("flanking")
@@ -105,31 +142,28 @@ haploblocks = def_blocks(ld = ld_pairs, map = map, method = "flanking",
 #turn the block object into a data frame
 haploblocks = block_obj_to_df(haploblocks, map)
 
+#summarize block information
+block_summary(block_df = haploblocks)
+
 #######################################
 #### Compute localGEBV  #####
 #######################################
 
-#the marker pecov (prediction error covariance matrix) is only needed if you want to compute
-#p-values of specific haplotype effects - very much theoretical at this point
-#the marker pecov structure is a matrix of prediction error (co)variances between the markers.
-#The rows and columns indicate markers and should be in the same order as marker effects
-#NOTE: set_missing_NA MUST be set to TRUE to evaluate this!
-
 #the first column in the marker effects file should be the SNP ID (same as the map file) and the second
 #column should be the marker effect estimated from a model.
-data("marker_effects", package = "FastStack")
+data("marker_effects", package = "HapSelect")
 
 
 
 #everything is as before, but set_missing_NA is an option that, if set to TRUE, sets any localGEBV with >= 1 missing genotype
 #to NA. If set to FALSE, it will compute localGEBV based on non-missing genotypes and only be NA if all genotypes are missing.
 
-#center = TRUE is the default and in the vast majority of cases needed. When estimating markers, USUALLY the genotype matrix is centered.
+#mean_adjust = TRUE is the default and in the vast majority of cases needed. When estimating markers, USUALLY the genotype matrix is centered.
 #Thus, marker effects are relative to centered genotypes. This is always true for GBLUP back solve and I believe
 #is more or less always true for Bayesian alphabet models and rrBLUP. This is important to pay attention to
 #you MUST get this right to prevent biased localGEBV! Note: it will not affect block variance, but it will change localGEBV values and affect the GA step.
 
-#A good confirmation is to reconstruct GEBV from Zu, where Z is the marker matrix and u are the marker effects.
+#A good confirmation is to reconstruct GEBV from Zu, where Z is the centered marker matrix and u are the marker effects.
 #The mean should be 0 (or very close to it) and reflect GBLUP GEBV if using rrBLUP, BayesC, or GBLUP back solve methods.
 #If the reconstructed GEBV mean is meaningfully away from 0, it indicates the wrong marker matrix (i.e., needs to be centered)
 #is being used or the marker matrix was not centered when estimating marker effects.
@@ -142,7 +176,7 @@ data("marker_effects", package = "FastStack")
 # (usually 0/1/2 for diploid, up to 9 for polypoid). We currently support polyploidy up to a ploidy of 9.
 
 haploblock_obj = compute_local_GEBV(geno = geno, marker_effects = marker_effects, haploblocks_df = haploblocks,
-                                    set_missing_NA = TRUE, center = TRUE)
+                                    set_missing_NA = TRUE, mean_adjust = TRUE)
 ##################################
 ####  Visualizations  #####
 ##################################
@@ -185,37 +219,43 @@ ld_decay_plot
 #### Parent Selection with the GA ####
 ##############################################
 
-#select top 15 haploblocks (arbitrary) and perform the GA
+#There are three methods we have implemented to select haploblocks:
+#1: select the top n blocks based on Block_Var
+#2: select the top x% of blocks (round up)
+#3: select the top blocks explaining x% of the total sum of block variance (round up)
 
-haploblock_effects = haploblock_obj$Haploblocks
 
-#order the blocks by block variance
-haploblock_effects = haploblock_effects[order(haploblock_effects$Block_Var, decreasing = TRUE), ]
+#1: select top 15 haploblocks (arbitrary)
 
-#select the top 15
-haploblock_top_blocks = haploblock_effects[1:15, ]
+haploblock_obj = select_top_blocks(haploblock_obj = haploblock_obj, n = 15)
 
-#pull out the localGEBV corresponding to the top 15
-localGEBV = haploblock_obj$Haplotype_Effect_Matrix
-localGEBV = localGEBV[row.names(localGEBV) %in% haploblock_top_blocks$Block_ID, ]
+#Objects are added to the haploblock_obj for the GA
+head(haploblock_obj$Haploblocks_GA)
+head(haploblock_obj$Haplotype_Effect_Matrix_GA)
+nrow(haploblock_obj$Haploblocks_GA)
 
-#transpose it and turn it into a matrix for the GA, blocks MUST be columns and individuals must be rows!
-localGEBV = as.data.frame(t(as.matrix(localGEBV)))
+#2 select top 50% of haploblocks
+haploblock_obj = select_top_blocks(haploblock_obj = haploblock_obj, perc_total = 0.5)
+nrow(haploblock_obj$Haploblocks_GA)
+
+#3 select the top blocks explaining at least 90% of the total block variance
+haploblock_obj = select_top_blocks(haploblock_obj = haploblock_obj, perc_of_total_var = 0.9)
+nrow(haploblock_obj$Haploblocks_GA)
 
 
 #run the GA
-#n_founders is the number of parents you want to select
-#popSize is the number of groups of 20 you want - more popSize takes longer per iteration, but leads to faster convergence
-#maxiter is the maximum number of iterations to reach an optimum solution
-#run is the maximum number of iterations without improvement before the GA stops
-#selfing determines whether a parent's localGEBV can be passed down to offspring twice (inbreeding with selfing) in the fitness function
-#pmutation is the probability a random individual from each population (popSize) of size n_founders will be substituted with a random individual
-#in the entire population (not including those already in the set of n_founders). Helps reach convergence more quickly, but too large a value may hinder finding the optimum
-#pcrossover is the probability of two populations (popSize) of n_founders exchanging half of their individuals - if the unique set of individuals after swapping is less than n_founders per population
-#then a random subset is sampled from the overall population like with pmutation (i.e., not in the unique set of individuals)
-#pelite when pulling random subsets from the base population after pcrossover, pelite will only consider the pelite proportion individuals with the greatest GEBV (fitness)
-#set to 1 to consider all individuals
-GA_output = genetic_algorithm(localGEBV = localGEBV, n_founders = 20, popSize = 10, maxiter = 300,
+#n_founders  is the number of parents you want to select
+#popSize     is the number of groups of 20 you want - more popSize takes longer per iteration, but leads to faster convergence
+#maxiter     is the maximum number of iterations to reach an optimum solution
+#run         is the maximum number of iterations without improvement before the GA stops
+#selfing     determines whether a parent's localGEBV can be passed down to offspring twice (inbreeding with selfing) in the fitness function
+#pmutation   is the probability a random individual from each population (popSize) of size n_founders will be substituted with a random individual
+#            in the entire population (not including those already in the set of n_founders). Helps reach convergence more quickly, but too large a value may hinder finding the optimum
+#pcrossover  is the probability of two populations (popSize) of n_founders exchanging half of their individuals - if the unique set of individuals after swapping is less than n_founders per population
+#            then a random subset is sampled from the overall population like with pmutation (i.e., not in the unique set of individuals)
+#pelite      when pulling random subsets from the base population after pcrossover, pelite will only consider the pelite proportion individuals with the greatest GEBV (fitness)
+#            set to 1 to consider all individuals
+GA_output = genetic_algorithm(localGEBV = haploblock_obj$Haplotype_Effect_Matrix_GA, n_founders = 20, popSize = 10, maxiter = 300,
                               run = 150, selfing = FALSE, pmutation = 0.2, pcrossover = 0.8, pelite = 0.5)
 
 #one unique set of parents - other solutions may exist, see the GA_output$GA@solution object for other potential sets of parents with the same fitness
