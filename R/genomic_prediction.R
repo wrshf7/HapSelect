31de9ecc4bebe90750896cdb6ded51285362dcde
wrshf7 <wrshf7@gmail.com>
2026-05-08 17:11:22 +1000

    Updates to the genomic prediction to compute narrow-sense heritability and add more explicit input checks. Also added more reporting.

--- R/genomic_prediction.R
+++ R/genomic_prediction.R
@@ -12,7 +12,11 @@ check_BLUE = function(BLUE, geno){
 }
 
 #solve the marker effects
-solve_marker_effects = function(geno, BLUE){
+solve_marker_effects = function(geno, BLUE, h2_method, ploidy){
+
+  if(length(ploidy) != 1 || is.na(ploidy) || is.null(ploidy) || !is.integer(ploidy)){
+    stop("Please provide a singular integer value for the ploidy. This can be specified using the #L format, where # is the ploidy number (2L is the default).")
+  }
 
   if(!check_BLUE(BLUE, geno)){
     stop("Ensure the first column is the exact genotype name in the genotype file and the second column is a singular adjusted phenotype, singular phenotype, BLUE, or de-regressed BLUP (no additional fixed or random effects can be fit).")
@@ -30,13 +34,36 @@ solve_marker_effects = function(geno, BLUE){
   geno_mat = t(geno[, BLUE[,1]])
 
   #allele frequencies
-  p = colMeans(geno_mat, na.rm = TRUE)
+  means = colMeans(geno_mat, na.rm = TRUE)
+
+  ploidy = max(geno_mat)
+
+  p = means / ploidy
+
+  # scaling for h2
+  if(length(h2_method) == 1 && h2_method == "VanRaden"){
+
+    h2_scale = ploidy * sum(p * (1-p))
+
+  } else if(length(h2_method) == 1 && h2_method == "marker_num"){
+
+    h2_scale = length(p)
+
+  } else stop("Please specify a valid method to compute the total additive genetic variance: either 'VanRaden' or 'marker_num'.")
 
   # center
-  Z = sweep(geno_mat, 2, p, "-")
+  Z = sweep(geno_mat, 2, means, "-")
 
   #solve marker effects
   marker_solutions = rrBLUP::mixed.solve(y = BLUE[,2], Z = Z)
+
+  marker_variance = marker_solutions$Vu
+  residual_variance = marker_solutions$Ve
+  additive_genetic_variance = marker_variance * h2_scale
+  h2 = additive_genetic_variance / (additive_genetic_variance + residual_variance)
+
+  cat(sprintf("Marker Variance:           %.4e \nAdditive Genetic Variance: %.4e \nResidual Variance:         %.4e \nh2:                        %.2f \n",
+              marker_variance, additive_genetic_variance, residual_variance, h2))
 
   return(marker_solutions)
 }
@@ -65,12 +92,15 @@ compute_prediction_accuracy = function(geno, marker_effects, BLUE){
 }
 
 #obtain data frame needed for next steps
-create_marker_effects_file = function(geno, BLUE){
-  marker_solutions = solve_marker_effects(geno, BLUE)
+create_marker_effects_file = function(geno, BLUE, h2_method = c("VanRaden", "marker_num"), ploidy = 2L){
+
+  h2_method = match.arg(h2_method)
+
+  marker_solutions = solve_marker_effects(geno, BLUE, h2_method, ploidy)
 
 
   prediction_accuracy = compute_prediction_accuracy(geno, marker_solutions, BLUE)
-  cat(paste0("Prediction Accuracy (cor(GEBV, BLUE or deregressed BLUP or adj pheno)) is: ", round(prediction_accuracy,2)))
+  cat(paste0("\nPrediction Accuracy (cor(GEBV, BLUE or deregressed BLUP or adj pheno)) is: ", round(prediction_accuracy,2)))
 
   #output to return
   output_df = data.frame(
@@ -80,48 +110,61 @@ create_marker_effects_file = function(geno, BLUE){
 }
 
 #perform cross-validation to assess prediction accuracy
-n_fold_cross_validation = function(geno, BLUE, nfold = 5){
+n_fold_cross_validation = function(geno, BLUE, nfold = 5L, h2_method = c("VanRaden", "marker_num"), ploidy = 2L){
   if(nrow(BLUE) <= 200){
     warning("Trying to do CV on a small population size may not work well!\n")
   }
+
+  if(length(nfold) > 1 || is.na(nfold) || !is.integer(nfold)){
+    stop("Provide a valid integer designation for the number of folds, nfold, in #L format (e.g., 5L).")
+  }
+
+  h2_method = match.arg(h2_method)
+
   fold_id = sample(rep(1:nfold, length.out = nrow(BLUE)))
 
   CV = sapply(1:nfold, function(fold){
-    cat(paste0("Performing cross-validation for fold: ", fold, "\n"))
+    cat(paste0("\nPerforming cross-validation for fold: ", fold, "\n"))
     train_BLUE = BLUE[fold_id != fold, ]
     valid_BLUE = BLUE[fold_id == fold, ]
 
-    marker_solutions = solve_marker_effects(geno, train_BLUE)
+    marker_solutions = solve_marker_effects(geno, train_BLUE, h2_method, ploidy)
     prediction_accuracy = compute_prediction_accuracy(geno, marker_solutions, valid_BLUE)
     return(prediction_accuracy)
   })
 
   mean_CV = mean(CV)
 
-  cat(paste0(nfold, " fold CV Mean Prediction Accuracy is: ", round(mean_CV, 2), "\n"))
+  cat(paste0("\n",nfold, " fold CV Mean Prediction Accuracy is: ", round(mean_CV, 2), "\n"))
 
   return(CV)
 }
 
-cross_validation = function(geno, BLUE, train_prop = 0.9, fold = 30){
+cross_validation = function(geno, BLUE, train_prop = 0.9, fold = 30, h2_method = c("VanRaden", "marker_num"), ploidy = 2L){
   if(nrow(BLUE) <= 200){
     warning("Trying to do CV on a small population size may not work well!\n")
   }
 
+  if(length(fold) > 1 || is.na(fold) || !is.integer(fold)){
+    stop("Provide a valid integer designation for the number of folds, nfold, in #L format (e.g., 5L).")
+  }
+
+  h2_method = match.arg(h2_method)
+
   CV = sapply(1:fold, function(fold){
-    cat(paste0("Performing cross-validation for fold: ", fold, "\n"))
+    cat(paste0("\nPerforming cross-validation for fold: ", fold, "\n"))
     id = sample(1:nrow(BLUE), ceiling(nrow(BLUE) * train_prop))
     train_BLUE = BLUE[id, ]
     valid_BLUE = BLUE[!(1:nrow(BLUE) %in% id), ]
 
-    marker_solutions = solve_marker_effects(geno, train_BLUE)
+    marker_solutions = solve_marker_effects(geno, train_BLUE, h2_method, ploidy)
     prediction_accuracy = compute_prediction_accuracy(geno, marker_solutions, valid_BLUE)
     return(prediction_accuracy)
   })
 
   mean_CV = mean(CV)
 
-  cat(paste0(fold, " fold CV Mean Prediction Accuracy is: ", round(mean_CV, 2), "\n"))
+  cat(paste0("\n",fold, " fold CV Mean Prediction Accuracy is: ", round(mean_CV, 2), "\n"))
 
   return(CV)
 }
